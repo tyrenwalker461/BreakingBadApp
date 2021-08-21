c45a8e4d8e6561e9e8c1c3a08f6c58bb1f28dc44
Tyren Walker <tyrenwalker461@gmail.com>
2021-08-21 18:20:37 -0400

    Initial Commit

---
+++ Shared/BreakingBadApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreakingBadApp.swift
+//  Shared
+//
+//  Created by MaKenna Dalcour on 8/21/21.
+//
+
+import SwiftUI
+
+@main
+struct BreakingBadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
