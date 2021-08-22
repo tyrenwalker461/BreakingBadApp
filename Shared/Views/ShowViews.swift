72d08062c095019de3daf77241dd14691b43ef58
Tyren Walker <tyrenwalker461@gmail.com>
2021-08-22 01:04:20 -0400

    basically done

---
+++ Shared/Views/ShowViews.swift
@@ -0,0 +1,20 @@
+//
+//  ShowViews.swift
+//  BreakingBad
+//
+//  Created by MaKenna Dalcour on 8/22/21.
+//
+
+import SwiftUI
+
+struct ShowViews: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ShowViews_Previews: PreviewProvider {
+    static var previews: some View {
+        ShowViews()
+    }
+}
